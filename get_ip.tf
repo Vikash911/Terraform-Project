b40e84e74dbc5d7fb104d8d511dfa95ab401f06a
Vikash Bhade <vikashbhade911@gmail.com>
2023-03-06 14:48:25 +0530

    chan14q

--- get_ip.tf
+++ get_ip.tf
@@ -7,6 +7,6 @@ resource "null_resource" "get_ip" {
 		command = "echo ${aws_instance.my_ec2.public_ip} > /home/ec2-user/myfile.txt"
 	}
 
-	depends_on =[aws_volume_attachment.ebs_attach"]
+	depends_on =[aws_volume_attachment.ebs_attach]
 
 }
