257d117a2f889689b4b7552021c4dd7bb205b3ec
Vikash Bhade <vikashbhade911@gmail.com>
2023-03-06 16:00:44 +0530

    s

--- get_ip.tf
+++ get_ip.tf
@@ -5,7 +5,7 @@ resource "null_resource" "get_ip"{
 	
 
 	provisioner "local-exec" {
-		command = "sudo sh -c ' echo ${aws_instance.my_ec2[0].public_ip} > /home/ec2-user/myfile.txt'"
+		command = "sh -c ' echo ${aws_instance.my_ec2[0].public_ip} > /home/ec2-user/myfile.txt'"
 		}
 
 depends_on = [aws_vpc.my-vpc, aws_internet_gateway.MyGateway9, aws_subnet.subnets, aws_route_table.RTable, aws_route_table_association.route_asso, aws_security_group.allow_ssh1,aws_instance.my_ec2]
