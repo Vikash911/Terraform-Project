dddedc47dca346ea428ee7fb72e80d838731ed69
Vikash Bhade <vikashbhade911@gmail.com>
2023-03-17 15:58:20 +0530

    f

--- get_ip.tf
+++ get_ip.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "get_ip"{
 
 provisioner "local-exec" {
-        command = "echo ${aws_instance.my_ec2[0].public_ip} >> /home/ec2-user/myfile.txt"
+        command = "echo ${aws_instance.my_ec2[0].public_ip} >> myfile.txt"
                 }
 }
 
