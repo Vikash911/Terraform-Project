f8c2752ac78f3ebe25699a67f88aca9b3f508702
Vikash Bhade <vikashbhade911@gmail.com>
2023-03-22 13:23:24 +0530

    ami-change

--- ec2_launch_in_my_vpc9.tf
+++ ec2_launch_in_my_vpc9.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   count                  = 1
-  ami                    = "ami-005f9685cb30f234b"
+  ami                    = "ami-0c9978668f8d55984"
   instance_type          = "t2.micro"
   subnet_id              = element(aws_subnet.subnets.*.id, count.index)
   availability_zone      = "us-east-1a"
