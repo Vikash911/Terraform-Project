374d61bc12811f0f039c42c2b46e0628e9490f81
Vikash Bhade <82812943+Vikash911@users.noreply.github.com>
2023-05-10 09:36:59 +0530

    Update ec2_laun

--- ec2_launch_in_my_vpc9.tf
+++ ec2_launch_in_my_vpc9.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   count                  = 1
-  ami                    = "ami-0c9978668f8d55984"
+  ami                    = "ami-0889a44b331db0194"
   instance_type          = "t2.micro"
   subnet_id              = element(aws_subnet.subnets.*.id, count.index)
   availability_zone      = "us-east-1a"
